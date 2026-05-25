c1fc72e794fd5042b61e8bc5f441cd6638241ad5
Ian Lam <ljy200523@gmail.com>
2026-05-25 21:58:45 +0800

    Add 11b_cat_spec
    Note of the scope: "expect" is in the "it" block -
    not "describe/context"

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,22 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:tom) { described_class.new("tom", "Singapura Cat", "dark brown") }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'Cat responds to talk with "meow"' do
+    it "can respond" do
+      expect(tom.talk).to eql "meow"
+    end
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(tom).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(tom).to be_hiding
   end
 end
